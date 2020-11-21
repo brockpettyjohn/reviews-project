db3a19a37e726065cfb0224803df07c346bbedb3
Brock Pettyjohn <brockpettyjohn@gmail.com>
2020-11-21 15:33:36 -0700

    calculated counts

--- src/app/review-display/review-display.component.ts
+++ src/app/review-display/review-display.component.ts
@@ -12,22 +12,35 @@ export class ReviewDisplayComponent implements OnInit {
 
   constructor(private dataService: DataService) { }
 
-  averageReviewRating: string;
+  averageReviewRating: number;
   reviews: Review[] = [];
+  ratingsCounts: any = {};
 
 
   ngOnInit(): void {
     this.dataService.getReviews().subscribe(rev => {
-      rev.forEach((r: Review) => this.reviews.push({...r, publish_date: new Date(r.publish_date).toLocaleDateString()}));
-      this.calculateAverageRating();
+      rev.forEach((r: Review) => this.reviews.push({ ...r, publish_date: new Date(r.publish_date).toLocaleDateString() }));
+      this.calculateTotalAverageRating();
+      this.calculateRatingCounts();
     });
   }
 
-  calculateAverageRating(): void {
+  calculateTotalAverageRating(): void {
     var addedRating = this.reviews.reduce((totalRatingsAdded, current) => {
       totalRatingsAdded += current.rating
       return totalRatingsAdded
     }, 0) / this.reviews.length
-    this.averageReviewRating = addedRating.toFixed(2);
+    this.averageReviewRating = parseFloat(addedRating.toFixed(2));
+  }
+
+  calculateRatingCounts(): void {
+    this.reviews.forEach(review => {
+      const rating = Math.floor(review.rating)
+      if (this.ratingsCounts[rating]) {
+        this.ratingsCounts[rating]++;
+      } else {
+        this.ratingsCounts[rating] = 1;
+      }
+    });
   }
 }
