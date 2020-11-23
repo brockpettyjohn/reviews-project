190518bde74ab18081f1336505b502b3cc8739b3
Brock Pettyjohn <brockpettyjohn@gmail.com>
2020-11-23 00:12:16 -0700

    actual final

--- src/app/review-detail-modal/review-detail-modal.component.ts
+++ src/app/review-detail-modal/review-detail-modal.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { DataService } from '../data.service';
 import { NgbModal, NgbActiveModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { Review } from '../review';
+import { } from '@angular/core';
 
 
 @Component({
   selector: 'app-review-detail-modal',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './review-detail-modal.component.html',
   styleUrls: ['./review-detail-modal.component.scss']
 })
@@ -26,7 +28,10 @@ export class ReviewDetailModalComponent implements OnInit {
     this.dataService.getReview(this.id).subscribe(rev => {
       this.review = { ...rev, publish_date: new Date(rev.publish_date).toLocaleDateString() }
     })
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+    this.modalService.open(content, { 
+      ariaLabelledBy: 'modal-basic-title', 
+      windowClass: 'modal-custom'
+    }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
